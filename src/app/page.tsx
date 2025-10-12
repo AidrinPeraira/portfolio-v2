4579e36215fefed8520eda481b07812d8ab02850
Aidrin Peraira <aidrin.peraira@gmail.com>
2025-10-13 01:30:31 +0530

    hidden nav bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
           <Experience />
           <Education />
         </div>
-        <Navbar />
+        {/* <Navbar /> */}
       </BackgroundWaves>
     </div>
   );
